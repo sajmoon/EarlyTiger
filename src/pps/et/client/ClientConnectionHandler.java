ce70f0f1db9c10415cd85d05507026199fdf42f0
lindvi <lindvi@kth.se>
2013-03-03 21:44:31 +0100

    ClientChatHandler added

---
+++ src/pps/et/client/ClientConnectionHandler.java
@@ -0,0 +1,60 @@
+package pps.et.client;
+import java.io.*;
+import java.net.Socket;
+
+public class ClientConnectionHandler extends Thread{
+	
+	private Socket s;
+	private PrintStream out;
+	private BufferedReader indata;
+	private boolean active;
+	private String text;
+	
+	static BufferedReader in = null;
+
+	
+	public ClientConnectionHandler(String ip, int port){
+		
+		active = true;
+		indata = new BufferedReader(new InputStreamReader(System.in));
+		
+		try {
+			s = new Socket(ip,port);
+			System.out.println("Connected");
+		} catch (Exception e) {
+			System.err.println("Connection refused");
+		}				
+		
+		try {
+			out = new PrintStream(s.getOutputStream());	
+		} catch (Exception e) {
+			System.err.println("Init error:" + e.getLocalizedMessage());
+		}
+	}
+
+	public static void main(String[] args) throws Exception{
+		ClientConnectionHandler c = new ClientConnectionHandler(args[0], Integer.parseInt(args[1]));
+		c.start();
+	}
+
+	public void run(){
+		while(s.isConnected()){
+			
+			// Listen if something is sent from the server
+			try{
+				in = new BufferedReader(new InputStreamReader(s.getInputStream()));	
+				System.out.println(in.readLine());
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+			
+			
+		}
+		System.out.println("Closing");
+	}
+	
+	
+	public void send(String message){
+		out.println(message);
+	}
+}

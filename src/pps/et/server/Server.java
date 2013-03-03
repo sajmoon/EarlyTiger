1e97d03c325a8721e541e7f320a19660e0f556c4
Simon Strom <simon.strom@gmail.com>
2013-03-03 20:55:23 +0100

    make it threaded

--- src/pps/et/server/Server.java
+++ src/pps/et/server/Server.java
@@ -3,17 +3,20 @@ package pps.et.server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 
 public class Server {
 	static int port = 4711;
 	static ServerSocket server;
-	Thread t;
+	static ArrayList<ServerConnectionHandler> connections;
 	
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws IOException {
+		
 		System.out.println("Starting server");
 		
-		Socket client;
+		connections = new ArrayList<ServerConnectionHandler>();
 		
 		try {
 			server = new ServerSocket(port);
@@ -23,17 +26,24 @@ public class Server {
 			
 		} catch (IOException e) {
 			System.out.println("Could not connect on port");
-		}	
+		} finally {
+			server.close();
+		}
 	}
 	
 	public static void listen(){
+		System.out.println("Waiting for connections..");
+		
 		Socket tempSocket = null;
 		try {
 			if((tempSocket = server.accept()) != null){
 				System.out.println("Accepted: " + tempSocket.getInetAddress());
 				ServerConnectionHandler c = new ServerConnectionHandler(tempSocket);
+				connections.add(c);
+				Thread t = new Thread(c);
 				
-				c.run();
+				t.start();
+				
 				System.out.println("started thread");
 				
 			}
